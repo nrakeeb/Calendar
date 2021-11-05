32e846d31b91d2c903fe1b5d5598f339a53e304f
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-11-05 15:21:39 +0000

    feat: read and write data to local storage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,3 +23,26 @@ function checkColour() {
         }
     }
 }
+
+$(".saveBtn").on("click", saveData);
+
+// save to local storage
+function saveData(event) {
+    var text = $(event.target).siblings(".description").val();
+    var id = $(this).parent().attr("id");
+    localStorage.setItem(id, text);
+    loadData();
+}
+
+// read from local storage
+function loadData() {
+    $('#9 .description').val(localStorage.getItem('9'));
+    $('#10 .description').val(localStorage.getItem('10'));
+    $('#11 .description').val(localStorage.getItem('11'));
+    $('#12 .description').val(localStorage.getItem('12'));
+    $('#13 .description').val(localStorage.getItem('13'));
+    $('#14 .description').val(localStorage.getItem('14'));
+    $('#15 .description').val(localStorage.getItem('15'));
+    $('#16 .description').val(localStorage.getItem('16'));
+    $('#17 .description').val(localStorage.getItem('17'));
+}
