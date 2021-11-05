e910bacf61acf11a56d566f4d38c6bc78036423d
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-11-05 15:20:37 +0000

    feat: change colour of time blocks based on current time

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,3 +8,18 @@ var currentTime = today.format("H");
 currentTime = parseInt(currentTime);
 
 timeEl.text(today.format("dddd, MMMM Do"));
+
+// change colour of time boxes depending on current time
+function checkColour() {
+    for (var i = 0; i < timeBlocks.length; i++) {
+        if (timeBlocks[i].id < currentTime) {
+            timeBlocks[i].classList.add("past");
+        }
+        else if (timeBlocks[i].id == currentTime) {
+            timeBlocks[i].classList.add("present");
+        }
+        else {
+            timeBlocks[i].classList.add("future");
+        }
+    }
+}
